c4d203d47fd8cd1a614195c7be5c9837d62177cf
Arindam <arinmupenn@gmail.com>
2019-05-31 12:51:32 -0700

    Change test code

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import process from process;
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+  process.exit(0);
 });
